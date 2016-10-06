2dfe77164d973db42d8d10ed862896ab2f6aa5cc
Veronika Stephanie <Veronika Stephanie>
2016-10-06 22:40:51 +0700

    first attemp

---
+++ Final Project/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int
+    option;
+
+    cout << "1. Interest"<<endl<<"2. Depreciation"<<endl;
+    cout << "Please choose the option you want "<<endl;
+    cin >> option;
+
+    return 0;
+}
