bdb3e91bc3678525afbc8d0f93a663ea4fa2c907
Veronika Stephanie <Veronika Stephanie>
2016-10-13 22:17:47 +0700

    Today I make the change for the menu and the option, then adding some comment

--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -1,27 +1,49 @@
 #include <iostream>
+#include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
+void interestChoices ();
+void depreciationChoices ();
+int singleInterest (double, double, double, double);
+double compoundInterest (double, double, double, double);
+int depreciationGoodValue ();
+int depreciationBookValue ();
+
+int interestOption,
+    depreciationOption;
+
+double  valueOwned1,
+        valueOwned2;
+
 int main()
 {
-    int
-    option;
+    int option;
 
-    cout << "1. Interest"<<endl<<"2. Depreciation"<<endl;
-    cout << "Please choose the option you want "<<endl;
+    //asking the user to choose the option
+    cout << "1. Interest" << endl << "2. Depreciation" << endl;
+    cout << "Please choose the option you want " << endl;
     cin >> option;
 
-    while (option<1 ||option>2){
-        cout<<"Please input the correct number"<<endl;
-        cin>>option;
+    //The condition if the users input the wrong option or the right option
+    while (option < 1 || option > 2){
+        cout << "Please input the correct number" << endl;
+        cin >> option;
     }
     while (option == 1){
-        cout<< "Welcome to the interest section"<<endl;
-        option += 2;
+        cout << "Welcome to the interest section" << endl;
+        break;
     }
     while (option == 2){
-            cout<< "Welcome to the depreciation section"<<endl;
-            option++;
+            cout << "Welcome to the depreciation section" << endl;
+            break;
     }
+
+    //The condition if the users choose the option
+    if (option == 1){
+        interestChoices();
+    }
+
     return 0;
 }
